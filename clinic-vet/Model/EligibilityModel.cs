eb8e5e044f13b10d1ee5f8f0f454e5588be89a30
Alana <Alana@DESKTOP-E6MEADU>
2023-11-04 16:16:25 -0300

    Adicionar arquivos de projeto.

---
+++ clinic-vet/Model/EligibilityModel.cs
@@ -0,0 +1,11 @@
+﻿namespace clinic_vet.Model
+{
+    [Serializable]
+    public class EligibilityModel
+    {
+            public Guid Id { get; set; }
+            public string Pet { get; set; }
+            public int NumeroPlano { get; set; }
+        
+    }
+}
